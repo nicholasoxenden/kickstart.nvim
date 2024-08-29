36182cec9f51b3350d950f436a85d5eb7fc2be4b
Nicholas Oxenden <nicholas.oxenden@gmail.com>
2024-08-29 10:47:26 -0600

    Minor updates

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,7 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+    vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Git' })
+    vim.keymap.set('n', '<leader>gd', vim.cmd.Gdiffsplit, { desc = 'Diff file' })
   end,
 }
--- lua/plugins/pomo.lua
+++ lua/plugins/pomo.lua
@@ -6,17 +6,5 @@ return {
   dependencies = {
     'rcarriga/nvim-notify',
   },
-  opts = {
-    -- run session with :TimerSession [session]
-    sessions = {
-      pomodoro = {
-        { name = 'Work', duration = '25m' },
-        { name = 'Short Break', duration = '5m' },
-        { name = 'Work', duration = '25m' },
-        { name = 'Short Break', duration = '5m' },
-        { name = 'Work', duration = '25m' },
-        { name = 'Long Break', duration = '15m' },
-      },
-    },
-  },
+  opts = {},
 }
